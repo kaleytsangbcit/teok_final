a4da2524e6d1e80625bfd0a4f717210a81307261
ykim312 <ykim312@my.bcit.ca>
2022-04-11 11:24:00 -0700

    teok to me title

--- teok/pages/index.js
+++ teok/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          It's Teok Final Project
+          Teok To Me
         </h1>
 
 
